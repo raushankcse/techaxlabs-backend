8d12503d101032a98d5fd0091d8c8f9b5c679236
Raushan Kumar <raushankcse@gmail.com>
2024-06-19 13:58:39 +0530

    cors

--- server.js
+++ server.js
@@ -6,10 +6,28 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+
+const allowedOrigins = ['https://techaxlabs-frontend.vercel.app'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
 
 
 import routes from './Routes/index.js'
